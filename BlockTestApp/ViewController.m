1757af80f19bcdafbdc27a7a12a088c66fdc7712
cimain <cimain@qq.com>
2018-04-23 12:04:25 +0800

    1

--- BlockTestApp/ViewController.m
+++ BlockTestApp/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Foundation/Foundation.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,70 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self testBlockForHeapOfARC];
+}
+
+-(void)testBlockForHeapOfARC{
+    int val =10;
+    typedef void (^blk_t)(void);
+    blk_t block = ^{
+        NSLog(@"blk0:%d",val);
+    };
+    block();
+}
+
+#pragma mark - testBlockForHeap0 - crash
+-(NSArray *)getBlockArray0{
+    int val =10;
+    return [NSArray arrayWithObjects:
+            ^{NSLog(@"blk0:%d",val);},
+            ^{NSLog(@"blk1:%d",val);},nil];
+}
+
+
+-(void)testBlockForHeap0{
+    
+    NSArray *tempArr = [self getBlockArray0];
+    NSMutableArray *obj = [tempArr mutableCopy];
+    typedef void (^blk_t)(void);
+    blk_t block = (blk_t){[obj objectAtIndex:0]};
+    block();
+}
+
+
+#pragma mark - testBlockForHeap1
+-(void)testBlockForHeap1{
+    
+    NSArray *tempArr = [self getBlockArray1];
+    NSMutableArray *obj = [tempArr mutableCopy];
+    typedef void (^blk_t)(void);
+    blk_t block = (blk_t){[obj objectAtIndex:0]};
+    block();
+}
+
+-(NSArray *)getBlockArray1{
+    int val =10;
+    return [NSArray arrayWithObjects:
+            [^{NSLog(@"blk0:%d",val);} copy],
+            [^{NSLog(@"blk1:%d",val);} copy],nil];
+}
+
+#pragma mark - testBlockForHeap2
+- (void)testBlockForHeap2{
+    
+    NSMutableArray *array = [NSMutableArray array];
+    [self addBlockToArray:array];
+    typedef void (^blk_t)(void);
+    blk_t block = (blk_t){[array objectAtIndex:0]};
+    block();
+}
+
+- (void)addBlockToArray:(NSMutableArray *)array {
+
+    int val =10;
+    [array addObjectsFromArray:@[
+         ^{NSLog(@"blk0:%d",val);},
+         ^{NSLog(@"blk1:%d",val);}]];
 }
 
 
--- BlockTestApp/main.m
+++ BlockTestApp/main.m
@@ -6,6 +6,7 @@
 //  Copyright © 2018年 cimain. All rights reserved.
 //
 
+//For checking memory management of block
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
@@ -14,3 +15,20 @@ int main(int argc, char * argv[]) {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+
+
+
+////For Clang rewrite
+//#include <stdio.h>
+
+//int main(int argc, char * argv[]) {
+//    @autoreleasepool {
+//
+//        typedef void (^blk_t)(void);
+//        blk_t block = ^{
+//            printf("Hello, World!\n");
+//        };
+//        block();
+//    }
+//}
+
